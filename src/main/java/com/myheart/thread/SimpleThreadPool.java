13a88f4cdf4b53a8f7adaba49fc01d8758720e13
Myheart <45277783+MMyheart@users.noreply.github.com>
2018-12-10 15:51:26 +0800

    建立简单项目结构

---
+++ src/main/java/com/myheart/thread/SimpleThreadPool.java
@@ -0,0 +1,9 @@
+package com.myheart.thread;
+
+/**
+ * @author yangxin
+ * @date 2018/12/10
+ */
+public class SimpleThreadPool extends ThreadPool {
+
+}
---
+++ src/main/java/com/myheart/thread/ThreadPool.java
@@ -0,0 +1,27 @@
+package com.myheart.thread;
+
+/**
+ * @author yangxin
+ * @date 2018/12/10
+ */
+public class ThreadPool {
+
+    private int maxThread = 1;
+
+
+
+
+
+    class SimpleTask implements Runnable {
+
+        public SimpleTask(){
+
+        }
+
+        @Override
+        public void run() {
+
+        }
+    }
+
+}
